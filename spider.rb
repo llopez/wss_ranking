5bfe4f007e1fbf0f2e9850ce8c0da3a7882e8c99
Luis Lopez <luigibyte@gmail.com>
2015-08-09 18:38:30 -0300

    splited search column into ID and skater name

--- spider.rb
+++ spider.rb
@@ -11,7 +11,7 @@ file.close
 
 begin 
   row = parts.shift(8).map(&:strip)
-  data << {name: row[3], country: row[4], position: row[0]}
+  data << {id: row[3].split[0], name: row[3].split[1..-1].join(' '), country: row[4], position: row[0]}
 end while !parts.empty?
 
 text = File.open('public/data/ranking.json', 'w')
