b22ab6f6cf1de213e57fb6cb3812dc06cf357d8e
Luis Lopez <luigibyte@gmail.com>
2015-08-09 23:04:40 -0300

    bk commit

--- js/app.js
+++ js/app.js
@@ -1,53 +1,65 @@
+window.App = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  init: function(){
+    // init
+  }
+}
+
+App.Models.Position = Backbone.Model.extend({});
+
+App.Collections.MenRanking = Backbone.Collection.extend({
+  model: App.Models.Position,
+  url: '/data/men.json'
+});
+
+App.Collections.WomenRanking = Backbone.Collection.extend({
+  model: App.Models.Position,
+  url: '/data/women.json'
+});
+
+App.Views.List = Backbone.View.extend({
+  el: 'table',
+  initialize: function(){
+    this.collection.on('add', this.addItem, this);
+    this.collection.fetch();
+  },
+  addItem: function(m){
+    row = _.template($('#item-template').html());
+
+    this.$el.find('tbody').append(row(m.toJSON()));
+  },
+  filter: function(name){
+    this.clear();
+    regexp = new RegExp(name, 'i');
+    results = this.collection.filter(function(m){
+      return m.get('name').match(regexp);
+    });
+
+    _.each(results, function(m){
+      this.addItem(m);
+    }, this);
+  },
+  clear: function(){
+    this.$el.find('tbody').html('');
+  }
+});
+
+App.Views.Search = Backbone.View.extend({
+  el: 'form#search',
+  events: {
+    'keyup input':'filter'
+  },
+  filter: function(e){
+    name = e.target.value;
+    list.filter(name);
+  }
+});
+ 
 $(function(){
-  var Position = Backbone.Model.extend({});
-  
-  var Ranking = Backbone.Collection.extend({
-    model: Position,
-    url: '/data/ranking.json'
-  });
-
-  ranking = new Ranking;
-
-  var List = Backbone.View.extend({
-    el: 'table',
-    initialize: function(){
-      this.collection.on('add', this.addItem, this);
-      this.collection.fetch();
-    },
-    addItem: function(m){
-      row = _.template($('#item-template').html());
-
-      this.$el.find('tbody').append(row(m.toJSON()));
-    },
-    filter: function(name){
-      this.clear();
-      regexp = new RegExp(name, 'i');
-      results = this.collection.filter(function(m){
-        return m.get('name').match(regexp);
-      });
-
-      _.each(results, function(m){
-        this.addItem(m);
-      }, this);
-    },
-    clear: function(){
-      this.$el.find('tbody').html('');
-    }
-  });
-
-  list = new List({collection: ranking});
-  
-  var Search = Backbone.View.extend({
-    el: 'form#search',
-    events: {
-      'keyup input':'filter'
-    },
-    filter: function(e){
-      name = e.target.value;
-      list.filter(name);
-    }
-  });
-  
-  search = new Search;
+  men = new App.Collections.MenRanking();
+  women = new App.Collections.WomenRanking();
+  search = new App.Views.Search;
 });
 
