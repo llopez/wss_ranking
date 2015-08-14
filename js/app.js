fb785f3e51fa69b2b5ad44405fff2bb3b25fbf1b
Luis Lopez <luigibyte@gmail.com>
2015-08-14 17:48:37 -0300

    added filter by country

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ window.App = {
   Collections: {},
   Views: {},
   init: function(options){
-    if(options.gender == 'men')
-      list = new App.Views.List({collection: men});
-    else
-      list = new App.Views.List({collection: women});
+    if(options.gender == 'men'){
+      list = new App.Views.List({collection: men}, options.country);
+    }
+    else{
+      list = new App.Views.List({collection: women}, options.country);
+    }
     new App.Views.Search;
   }
 }
@@ -15,7 +17,11 @@ App.Models.Position = Backbone.Model.extend({});
 
 App.Collections.MenRanking = Backbone.Collection.extend({
   model: App.Models.Position,
-  url: '/data/men.json'
+  url: '/data/men.json',
+  /*byCountry: function(name){
+    result = this.filter(function(man){ return man.get('country') == name});
+    return new App.Collections.MenRanking(result);
+  }*/
 });
 
 App.Collections.WomenRanking = Backbone.Collection.extend({
@@ -25,11 +31,14 @@ App.Collections.WomenRanking = Backbone.Collection.extend({
 
 App.Views.List = Backbone.View.extend({
   el: 'table',
-  initialize: function(){
+  initialize: function(options, country){
+    this.country = country;
     this.collection.on('add', this.addItem, this);
     this.collection.fetch();
   },
   addItem: function(m){
+    if(this.country && m.get('country') != this.country)
+      return;
     row = _.template($('#item-template').html());
 
     this.$el.find('tbody').append(row(m.toJSON()));
@@ -37,6 +46,9 @@ App.Views.List = Backbone.View.extend({
   filter: function(name){
     this.clear();
     regexp = new RegExp(name, 'i');
+
+    //arg = this.collection.byCountry('ARG');
+
     results = this.collection.filter(function(m){
       return m.get('name').match(regexp);
     });
