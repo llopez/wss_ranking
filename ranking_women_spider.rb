6048cf6b70f3bfd8c4f6738da87a200f1b0f41b7
Luis Lopez <luigibyte@gmail.com>
2015-08-17 21:36:37 -0300

    improved woman ranking spider

--- ranking_women_spider.rb
+++ ranking_women_spider.rb
@@ -1,20 +1,18 @@
+# pdftohtml -enc UTF-8 -f 18 -l 28 ranking.pdf women.html
+
 require 'json'
 
 file = File.open('public/data/women.html')
 parts = []
 data = []
 while !file.eof?
-  begin
   line = file.readline
-  parts << line.gsub(/<b>|<\/b>|<br\/>|<br>/, '').gsub("&#160;"," ")
-  rescue
-  puts line
-  end
+  parts << line.strip
 end
 file.close
 
 begin 
-  row = parts.shift(9).map(&:strip)
+  row = parts.shift(9)
   data << {id: row[3], name: row[4], country: row[5], position: row[0]}
 end while !parts.empty?
 
