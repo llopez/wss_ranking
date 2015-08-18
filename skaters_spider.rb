df8829c01cb0f183736fa0bd68e4c19d435d8c9b
Luis Lopez <luigibyte@gmail.com>
2015-08-17 21:07:03 -0300

    added skaters spider, pdf, html and json

---
+++ skaters_spider.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+file = File.open('public/data/skaters-july-2015.html')
+parts = []
+data = []
+while !file.eof?
+  line = file.readline
+  parts << line.strip 
+end
+file.close
+
+begin 
+  row = parts.shift(3)
+  data << {id: row[0], name: row[1], country: row[2]}
+end while !parts.empty?
+
+text = File.open('public/data/skaters.json', 'w')
+text.write JSON.generate(data)
+text.close
+
